f8bc2007eede45f12e01812aa6fff6cb58c40fca
saumyapandyaa <64671058+saumyapandyaa@users.noreply.github.com>
2023-01-10 12:55:19 -0500

    add software

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,8 +9,8 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
               My past experiences have  allowed me strengthen my soft skills like problem-solving, communication, team-work, multi-tasking
-                    etc. I have plenty of experience working with technology. Some examples are Python, C,
-                    C++, Java, HTML, CSS, MySQL, PHP, JavaScript, SolidWorks, Multisim, Pygame, React, Matplotlib, Node js, Microsoft Office Applications.
+                    etc. I have plenty of experience working with technology. Some examples are <b className="purple" >Python, C,
+                    C++, Java, HTML, CSS, MySQL, PHP, JavaScript, SolidWorks, Multisim, Pygame, React, Matplotlib, Node js, Microsoft Office Applications, Bash, Powershell.</b>
                     I am extremely passionate about technology. I cannot wait to solve more and more realistic problems
                     and contribute to the tech industry. Apart from coding, I also enjoy
           </p>
