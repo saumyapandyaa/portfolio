79320a252f99801f47e3b177416c834ee960f0be
saumyapandyaa <64671058+saumyapandyaa@users.noreply.github.com>
2023-01-15 16:00:29 -0500

    removed contact

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import Contact from  "./components/Contact/contact";
 import {
   BrowserRouter as Router,
   Route,
