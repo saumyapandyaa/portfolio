34eccc8dee22dd261fac5576c87f7623e0020306
saumyapandyaa <64671058+saumyapandyaa@users.noreply.github.com>
2023-02-20 15:04:09 -0500

    additional experience

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -11,7 +11,7 @@ function AboutCard() {
               My past experiences have  allowed me strengthen my soft skills like problem-solving, communication, team-work, multi-tasking
                     etc. I have plenty of experience working with technology. Some examples are <b className="purple" >Python, C,
                     C++, Java, CSS, MySQL, PHP, JavaScript, Node.js, React.js, Microsoft Office Applications, Bash, Powershell.</b>
-                    I am extremely passionate about technology. I cannot wait to solve more and more realistic problems
+                    I am extremely passionate about technology. I have learnt about and worked using a really amazing software management approach like <b className="purple">Agile</b>. I cannot wait to solve more and more realistic problems
                     and contribute to the tech industry. Apart from coding, I also enjoy
          
           <ul>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -29,7 +29,7 @@ function Home2() {
                                   <li><b className="purple">Languages: </b>C, C++, Python, PHP, HTML, CSS, Java, Lua, MySQL, JavaScript, VHDL and scripting in Bash </li>
                                   <li><b className="purple">Tools: </b>VS Code, GitHub, Powershell, React.js, Node.js, Capstone, Pygame, Latex, 
 Solidworks, Maple, Matplotlib, Microsoft Office(Word, Excel, Outlook, Power Point),  Xilix ISE, Labview</li>
-                                 <li><b className="purple">Some concepts:</b>Object Oriented Programming, Complexity, Testing/Debugging, Recursion, Stacks, Queues(and Heap), Linked Lists, Trees, Hashing, Graphs</li><li><b className="purple">Relevant Coursework:</b> Web programming, Object Oriented Programming, Software Project 
+                                 <li><b className="purple">Some concepts and/or methodologies:</b> Scrum, Agile, Object Oriented Programming, Complexity, Testing/Debugging, Recursion, Stacks, Queues(and Heap), Linked Lists, Trees, Hashing, Graphs</li><li><b className="purple">Relevant Coursework:</b> Web programming, Object Oriented Programming, Software Project 
 Management, Digital Systems, Data Structures</li><br></br>
             <b className="purple"><u> Awards: </u></b>
                 <li>President's List Honours (2022-2023)</li>
